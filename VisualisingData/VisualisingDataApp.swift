ee07d7e0ffd197cf51c4995d7e15a79baf8ba7d5
Alistair Begg <begg.a@etoncollege.org.uk>
2022-03-03 12:18:27 +0000

    Initial Commit

---
+++ VisualisingData/VisualisingDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualisingDataApp.swift
+//  VisualisingData
+//
+//  Created by Begg, Alistair (AMM) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct VisualisingDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
